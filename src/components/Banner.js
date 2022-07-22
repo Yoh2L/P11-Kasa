b9176edb1fd62447108b98a5a5bc254581d9f5f4
Yohan <yohan.deletoille@gmail.com>
2022-07-22 11:21:14 +0200

    V0.1 - Debut architecture + header

---
+++ src/components/Banner.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Banner = () => {
+	return (
+		<div className="banner">
+			<img
+				className="banner_img"
+				src="./kasa_bg.png"
+				alt="Banniere d'accueil"
+			/>
+			<p className="banner_slogan">Chez vous, partout et ailleurs</p>
+		</div>
+	);
+};
+
+export default Banner;
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+
+const Home = () => {
+	return (
+		<div>
+			<header>
+				<Header />
+				<Banner />
+			</header>
+			<main></main>
+		</div>
+	);
+};
+
+export default Home;
